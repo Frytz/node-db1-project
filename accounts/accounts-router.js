fba00c1f0bcb0272b1de8657d09302367d7dc199
John Rossi <DACOOK1983@gamil.com>
2020-07-18 20:24:41 -0400

    mvp

--- accounts/accounts-router.js
+++ accounts/accounts-router.js
@@ -45,5 +45,43 @@ router.post('/', async (req, res) => {
     }
 });
 
+//PUT
+
+router.put('/:id', async (req, res) => {
+    const {id} = req.params;
+    const newAccount =req.body;
+    try {
+        const count = await knex('accounts').update(newAccount).where({id});
+        if (count) {
+            res.json({updated: count});
+        } else {
+            res.status(404).json({message: "Account not found"})
+        }
+    }
+    catch (err) {
+        console.log(err);
+        res.json(500).json({ message: "problem with db", error: err });
+    }
+});
+
+//DELETE
+
+router.delete('/:id', async (req, res) => {
+    const {id} = req.params;
+    try {
+        const count = await knex('accounts').del().where({id});
+        if (count) {
+            res.json({ deleted: count });
+        } else {
+            res.status(404).json({ message: "Account not found" })
+        }
+    } catch (err) {
+        console.log(err);
+        res.json(500).json({ message: "problem with db", error: err });
+    }
+
+});
+
+
 
 module.exports = router;
